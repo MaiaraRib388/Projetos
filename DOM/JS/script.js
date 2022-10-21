5e78586bb8bc16bc148e1fb9cf5b510d465d5d9b
MaiaraRib388 <113558275+MaiaraRib388@users.noreply.github.com>
2022-10-21 15:36:45 -0300

    Up now

---
+++ DOM/JS/script.js
@@ -0,0 +1,51 @@
+//getElementById()
+// const elementH2 = document.getElementById('myId');
+// console.log(elementH2);
+
+//querySelector('tag')
+// const MyHeader = document.querySelector('header');
+// console.log(MyHeader);
+
+//querySelector('#id')
+// const MyHeader = document.querySelector('#myId')
+// console.log(MyHeader);
+
+//querySelector('.class')
+// const MyHeader = document.querySelector('.myClass');
+// console.log(MyHeader);
+
+//querySelectorAll('.class')
+// const myP = document.querySelectorAll('.myClass');
+// console.log(myP);
+
+//createElement()
+// const myDiv = document.createElement('div');
+
+//apeend - final
+// const myBody = document.querySelector('Body');
+
+// myBody.append(myDiv);
+
+//createElement()
+// const myDiv = document.createElement('div');
+
+//prepend() - início
+// const myBody = document.querySelector('Body');
+
+// myBody.prepend(myDiv);
+
+//innerText
+// const myDiv = document.createElement('div');
+// myDiv.innerText = 'Eu sou uma div criada pelo JS';
+
+// const myBody = document.querySelector('body');
+// myBody.prepend(myDiv);
+
+// const myDiv = document.createElement('div');
+// myDiv.innerHTML = `<strong>Eu sou uma div criada pelo JS </strong>`
+
+// const myBody = document.querySelector('body');
+// myBody.prepend(myDiv);
+
+// const myH1 = document.querySelector('h1');
+// myH1.style.backgroundColor = 'pink';
---
+++ DOM/aula.md
@@ -0,0 +1,38 @@
+## DOM
+
+É uma forma que o JavaScript tem de se comunicar com o HTML.
+
+## Formas de pegar um elemento no HTML
+
+## getElement
+
+1.getElementById() - Chamar o ID do HTML.
+
+## querySelector
+
+1.querySelector('tag') Chamar a Tag
+
+2.querySelector('id') Chamar o ID
+
+3.querySelector('.class') Chamar a Class
+
+## querySelectorAll
+
+1.querySelectorAll('tag') Chamar todas as Tag's Selecionadas
+
+2.querySelectorAll('.class') Chamar todas as Class's Selecionadas
+
+## createElement
+* createElement() - Primeiro eu crio a nova Tag
+
+* apeend(); -> O novo elemento será inserido antes do fechamento da tag selecionada.
+
+* prepend(); -> O novo elemento será inserido após o início da tag selecionada.
+
+## Manipulação dos dados
+* innerText -> Modifica o conteúdo interno;
+
+* innerHTML -> Modifica o conteúdo interno e a formatação.
+
+## Manipulação de estilo
+* style -> Modofica o estilo da tag.
